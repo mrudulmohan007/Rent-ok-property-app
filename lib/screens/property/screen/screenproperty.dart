0ed92aacced903edeb69c573945724896cd79c52
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-10-03 15:30:01 +0530

    removed tabbar

--- lib/screens/property/screen/screenproperty.dart
+++ lib/screens/property/screen/screenproperty.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rental_ok_app/screens/property/Tabs/dashboardscreen.dart';
-import 'package:rental_ok_app/screens/property/Tabs/roomsscreen.dart';
-import 'package:rental_ok_app/screens/property/Tabs/listingscreen.dart';
-import 'package:rental_ok_app/screens/property/Tabs/complaintscreen.dart';
-import 'package:rental_ok_app/screens/property/Tabs/foodscreen.dart';
 
-class ScreenProperty extends StatefulWidget {
-  const ScreenProperty({super.key});
-
-  @override
-  State<ScreenProperty> createState() => _ScreenPropertyState();
-}
-
-class _ScreenPropertyState extends State<ScreenProperty>
-    with SingleTickerProviderStateMixin {
-  TabController? _tabController;
-  @override
-  void initState() {
-    _tabController = TabController(length: 5, vsync: this);
-  }
-
+class ScreenProperty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,40 +32,7 @@ class _ScreenPropertyState extends State<ScreenProperty>
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: TabBar(
-                controller: _tabController,
-                isScrollable: true, // Set to true for scrollable tabs
-                unselectedLabelColor: Colors.grey,
-                labelColor: Color.fromARGB(255, 4, 23, 233),
-                tabs: [
-                  Tab(
-                    text: 'Dashboard',
-                  ),
-                  Tab(text: 'Rooms'),
-                  Tab(text: 'Listing'),
-                  Tab(text: 'Complaint'),
-                  Tab(text: 'Food'),
-                ],
-              ),
-            ),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  DashboardTab(),
-                  RoomsTab(),
-                  ListingTab(),
-                  ComplaintTab(),
-                  FoodTab(),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: DashboardTab(),
       ),
     );
   }
